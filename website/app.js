3be1d1243cbe21babc187f7fe3469780c9bd2e4b
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-06-12 00:44:01 +0300

    feature: call to weather api.
    - get weather data from api.

--- website/app.js
+++ website/app.js
@@ -3,3 +3,33 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+let zipCode = '90001'; // los angeless.
+const apiKey = '63051696b741ea5504c120b377927c37';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Fetch data from the API
+const getweatherDataByZipCode = async (url = '') => {
+    const response = fetch(url, {
+        method:'GET',
+        headers:{
+            'content-type': 'application/json'
+        }
+    });
+
+    try {
+        const weatherData = await response.json();
+        console.log(weatherData);
+    }catch (e) {
+        console.log('Errors: ' + e);
+    }
+
+};
+
+const createWeatherURL = () => {
+    let url =  `${baseURL}?zip=${zipCode}&appid=${apiKey}`;
+    return url;
+};
+
+getweatherDataByZipCode(createWeatherURL());
+
